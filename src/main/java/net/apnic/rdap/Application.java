ce076031cd01536daff15461c01ff3a75c116a3a
Thomas Miller <tmiller@apnic.net>
2017-05-18 12:59:40 +1000

    Start of spring-boot application setup.

--- src/main/java/net/apnic/rdap/Application.java
+++ src/main/java/net/apnic/rdap/Application.java
@@ -1,8 +1,14 @@
 package net.apnic.rdap;
 
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
+import org.springframework.boot.SpringApplication;
+
+@EnableAutoConfiguration
 public class Application
 {
     public static void main(String[] args)
+        throws Exception
     {
+        SpringApplication.run(Application.class, args);
     }
 }
