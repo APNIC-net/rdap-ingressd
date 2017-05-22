f14ae161df029b8ccb0619819fe33b81ddf7757b
Thomas Miller <tmiller@apnic.net>
2017-05-23 08:46:46 +1000

    Documentation for PathRestController

--- src/main/java/net/apnic/rdap/path/rest/PathRestController.java
+++ src/main/java/net/apnic/rdap/path/rest/PathRestController.java
@@ -4,15 +4,21 @@ import net.apnic.rdap.client.RDAPClient;
 
 import org.springframework.beans.factory.annotation.Autowired;
 
+/**
+ * Abstract class for all RDAP rest controller path segments to subclass from.
+ *
+ * This class provides utilities that all path segment controllers will need.
+ */
 public abstract class PathRestController
 {
     @Autowired
     private RDAPClient rdapClient;
 
-    public PathRestController()
-    {
-    }
-
+    /**
+     * Provides the RDAP client to use for proxying requests.
+     *
+     * @return RDAPClient to used for proxying requests.
+     */
     public RDAPClient getRDAPClient()
     {
         return rdapClient;
