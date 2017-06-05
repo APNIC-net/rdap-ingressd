a97c65a3c262fbc74f27870daf4f6c4a875dd86e
Thomas Miller <tmiller@apnic.net>
2017-06-05 15:58:19 +1000

    Entity documentation

--- src/main/java/net/apnic/rdap/entity/Entity.java
+++ src/main/java/net/apnic/rdap/entity/Entity.java
@@ -1,14 +1,29 @@
 package net.apnic.rdap.entity;
 
+/**
+ * Represents a single entity object in RDAP
+ */
 public class Entity
 {
     private String entity;
 
+    /**
+     * Default construct
+     *
+     * Takes the entity name
+     *
+     * @param entity Entity name
+     */
     public Entity(String entity)
     {
         this.entity = entity;
     }
 
+    /**
+     * Returns the entity name.
+     *
+     * @return Entity name
+     */
     public String getEntity()
     {
         return entity;
