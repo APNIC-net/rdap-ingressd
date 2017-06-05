771a31fbf659515077094a36566a9526d9c101a6
Thomas Miller <tmiller@apnic.net>
2017-06-05 16:03:41 +1000

    Updated NameServer object to inherit from Domain.

--- src/main/java/net/apnic/rdap/nameserver/NameServer.java
+++ src/main/java/net/apnic/rdap/nameserver/NameServer.java
@@ -1,16 +1,18 @@
 package net.apnic.rdap.nameserver;
 
-public class NameServer
-{
-    private String nameServer;
+import net.apnic.rdap.domain.Domain;
 
+/**
+ * Represents a single name server object in RDAP
+ */
+public class NameServer
+    extends Domain
+{
+    /**
+     * {@inheritDoc}
+     */
     public NameServer(String nameServer)
     {
-        this.nameServer = nameServer;
-    }
-
-    public String getNameServer()
-    {
-        return nameServer;
+        super(nameServer);
     }
 }
