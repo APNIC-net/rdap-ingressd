7aea91e99b293cf0a84ef58ade40351695f5114e
Thomas Miller <tmiller@apnic.net>
2017-06-08 14:15:31 +1000

    Added enable toggle for IANA scrapper

--- src/main/java/net/apnic/rdap/iana/config/IANAConfiguration.java
+++ src/main/java/net/apnic/rdap/iana/config/IANAConfiguration.java
@@ -9,12 +9,14 @@ import net.apnic.rdap.resource.ResourceStore;
 import net.ripe.ipresource.IpRange;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.autoconfigure.condition.ConditionalOnProperty;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
 @Configuration
 public class IANAConfiguration
 {
+    @ConditionalOnProperty(value="rdap.scraping.scrapers.iana.enabled")
     @Bean
     @Autowired
     public IANABootstrapScraper ianaScraper(RDAPAuthorityStore authorityStore,
