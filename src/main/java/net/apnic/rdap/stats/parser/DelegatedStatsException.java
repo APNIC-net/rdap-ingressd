a624b44aa7704d486159017dde21b77a69aba090
Thomas Miller <tmiller@apnic.net>
2017-06-09 15:47:18 +1000

    Documentation for DelegatedStatsException

--- src/main/java/net/apnic/rdap/stats/parser/DelegatedStatsException.java
+++ src/main/java/net/apnic/rdap/stats/parser/DelegatedStatsException.java
@@ -1,8 +1,18 @@
 package net.apnic.rdap.stats.parser;
 
+/**
+ * Generic exception class that represents errors during delegated stats
+ * passing.
+ */
 public class DelegatedStatsException
     extends Exception
 {
+    /**
+     * Default constructor
+     *
+     * @param message Exception memssage
+     * @see java.lang.Exception
+     */
     public DelegatedStatsException(String message)
     {
         super(message);
