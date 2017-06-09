4d9e0f4a812a69ce78e5b1c156412e8be4af57cb
Thomas Miller <tmiller@apnic.net>
2017-06-09 15:55:54 +1000

    Documentation for Header class.

--- src/main/java/net/apnic/rdap/stats/parser/Header.java
+++ src/main/java/net/apnic/rdap/stats/parser/Header.java
@@ -2,6 +2,9 @@ package net.apnic.rdap.stats.parser;
 
 import org.apache.commons.csv.CSVRecord;
 
+/**
+ * Class represents a header record for a delegated stats file
+ */
 public class Header
     implements Line
 {
@@ -9,16 +12,41 @@ public class Header
 
     private String version;
 
+    /**
+     * Default constructor.
+     *
+     * Takes a CSV record that represents the header from a delegated stats
+     * file.
+     *
+     * @param record CSVRecord that contains the header information
+     * @throws IllegalArgumentException If CSVRecord is not a header
+     */
     public Header(CSVRecord record)
     {
+        if(fits(record) == false)
+        {
+            throw new IllegalArugmentException("Not a header record");
+        }
+
         this.version = record.get(0);
     }
 
+    /**
+     * Checks a given CSVRecord to see if it contains a delegated stats header.
+     *
+     * @param record CSVRecord to check
+     * @return True if record is a header
+     */
     public static boolean fits(CSVRecord record)
     {
         return record.size() == DEFAULT_RECORD_SIZE;
     }
 
+    /**
+     * Returns the version contained in the header.
+     *
+     * @return version string
+     */
     public String getVersion()
     {
         return version;
