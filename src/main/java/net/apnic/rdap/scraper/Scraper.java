7f7264b62c4c22e1c3acb6cab56f84043d76e967
Thomas Miller <tmiller@apnic.net>
2017-06-13 11:09:42 +1000

    Documentation for Scraper interface.

--- src/main/java/net/apnic/rdap/scraper/Scraper.java
+++ src/main/java/net/apnic/rdap/scraper/Scraper.java
@@ -2,7 +2,20 @@ package net.apnic.rdap.scraper;
 
 import java.util.concurrent.CompletableFuture;
 
+/**
+ * Generic interface that scrapers must conform to.
+ */
 public interface Scraper
 {
+    /**
+     * Name of the scraper used for debuging and error logging purposes.
+     */
+    public String getName();
+
+    /**
+     * Main worker method that gets called by the scheduler for each scraper.
+     *
+     * @return Future that is fulfilled when the scraper finishes it's tasks.
+     */
     public CompletableFuture<Void> start();
 }
