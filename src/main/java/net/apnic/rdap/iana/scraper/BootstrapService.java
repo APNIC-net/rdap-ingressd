bd73d1a669fbb8fa697faf9f411574fa45fc8cd4
Thomas Miller <tmiller@apnic.net>
2017-06-20 08:34:08 +1000

    IANA Scraping docs.

--- src/main/java/net/apnic/rdap/iana/scraper/BootstrapService.java
+++ src/main/java/net/apnic/rdap/iana/scraper/BootstrapService.java
@@ -7,6 +7,9 @@ import java.net.URISyntaxException;
 import java.util.ArrayList;
 import java.util.List;
 
+/**
+ * Represents a single service found in a given Bootstrap file.
+ */
 class BootstrapService
 {
     public static final int NUMBER_OF_ELEMENTS = 2;
@@ -16,6 +19,13 @@ class BootstrapService
     private List<URI> serversByURI = null;
     private List<List<String>> serviceElements = null;
 
+    /**
+     * Constructs a new service with the the supplied service elements from the
+     * bootstrap file.
+     *
+     * @param serviceElements Elements that comprise a single service found in a
+     *                        Bootstrap file
+     */
     @JsonCreator
     public BootstrapService(List<List<String>> serviceElements)
     {
@@ -31,16 +41,39 @@ class BootstrapService
         this.serviceElements = serviceElements;
     }
 
+    /**
+     * Returns the resources that this service depicts for a given Bootstrap
+     * file.
+     *
+     * @return List of resources and their string representations
+     */
     public List<String> getResources()
     {
         return serviceElements.get(RESOURCES_INDEX);
     }
 
+    /**
+     * Returns the server URI's where authorative information can be found for
+     * the resources described by this service.
+     *
+     * @return List of servers and their string representation
+     */
     public List<String> getServers()
     {
         return serviceElements.get(SERVERS_INDEX);
     }
 
+    /**
+     * Returns this services servers as actual URI objects.
+     *
+     * Users of this function must be read to catch errors as its plausible that
+     * a non conformant server URI could be passed which will result in an error
+     * being thrown.
+     *
+     * @return List of server and their URI representations
+     * @throws URISyntaxException for when a URI does not conformto URI standard
+     * @see getServers()
+     */
     public List<URI> getServersByURI()
         throws URISyntaxException
     {
--- src/main/java/net/apnic/rdap/iana/scraper/BootstrapVersionException.java
+++ src/main/java/net/apnic/rdap/iana/scraper/BootstrapVersionException.java
@@ -2,9 +2,20 @@ package net.apnic.rdap.iana.scraper;
 
 import java.util.List;
 
+/**
+ * Exception is thrown when a bootstrap file version is not supported or
+ * expected.
+ */
 public class BootstrapVersionException
     extends RuntimeException
 {
+    /**
+     * Takes the recieved version from the bootstrap file and a list of
+     * supported versions that where expected.
+     *
+     * @param recievedVersion Version recieved from the bootstrap file
+     * @param supportedVersions List of expected/supported versions
+     */
     public BootstrapVersionException(String recievedVersion,
                                      List<String> supportedVersions)
     {
