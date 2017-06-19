451c73db7c8cc31d0b4573efd75e74fbed13add4
Thomas Miller <tmiller@apnic.net>
2017-06-20 09:22:11 +1000

    Documentation for ResourceRecord

--- src/main/java/net/apnic/rdap/stats/parser/ResourceRecord.java
+++ src/main/java/net/apnic/rdap/stats/parser/ResourceRecord.java
@@ -2,6 +2,9 @@ package net.apnic.rdap.stats.parser;
 
 import org.apache.commons.csv.CSVRecord;
 
+/**
+ * Represents a single resource record in a delegated stats file
+ */
 public abstract class ResourceRecord
     implements Line
 {
@@ -13,6 +16,17 @@ public abstract class ResourceRecord
     String type = null;
     String value = null;
 
+    /**
+     * Constructor for a new ResourceRecord that isn't being constructed through
+     * a CSVRecord.
+     *
+     * @param registry The registry this resource record belongs to
+     * @param type Type string to depict the type of resource this record
+     *             contains.
+     * @param start Starting value for this type of resource record
+     * @param value Value that is matched with start for the given resource
+     *              record type
+     */
     public ResourceRecord(String registry, String type, String start,
                           String value)
     {
@@ -22,6 +36,11 @@ public abstract class ResourceRecord
         this.value = value;
     }
 
+    /**
+     * Constructs a new ResourceRecord from a CSVRecord
+     *
+     * @param record CSVRecord to construct this object from
+     */
     public ResourceRecord(CSVRecord record)
     {
         if(fits(record) == false)
@@ -35,27 +54,53 @@ public abstract class ResourceRecord
         value = record.get(4);
     }
 
+    /**
+     * Convience method that checks if a supplied CSVRecord fits the mold of a
+     * resource record.
+     *
+     * @param record CSVRecord to check
+     */
     public static boolean fits(CSVRecord record)
     {
         return record.size() >= MIN_RECORD_SIZE &&
                record.size() <= MAX_RECORD_SIZE;
     }
 
+    /**
+     * Returns the registry value for this record.
+     *
+     * @return Registry value
+     */
     public String getRegistry()
     {
         return registry;
     }
 
+    /**
+     * Returns the start value for this record
+     *
+     * @return Resource record start value
+     */
     public String getStart()
     {
         return start;
     }
 
+    /**
+     * Type value for this record
+     *
+     * @return Resource type value
+     */
     public String getType()
     {
         return type;
     }
 
+    /**
+     * Resource value that is used in conjunction with the start value
+     *
+     * @return Resource value
+     */
     public String getValue()
     {
         return value;
