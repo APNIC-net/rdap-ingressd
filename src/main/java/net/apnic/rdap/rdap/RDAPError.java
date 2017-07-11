8d289902852f22f3e20336b0d4d865e1b054efd3
Thomas Miller <tmiller@apnic.net>
2017-07-11 15:40:19 +1000

    Docs for RDAPError

--- src/main/java/net/apnic/rdap/rdap/RDAPError.java
+++ src/main/java/net/apnic/rdap/rdap/RDAPError.java
@@ -5,6 +5,11 @@ import com.fasterxml.jackson.annotation.JsonInclude;
 import java.util.ArrayList;
 import java.util.List;
 
+/**
+ * Class represent an error response in the RDAP protocol.
+ *
+ * @see https://tools.ietf.org/html/rfc7483
+ */
 public class RDAPError
     extends RDAPObject
 {
@@ -12,6 +17,12 @@ public class RDAPError
     private String errorCode = null;
     private String title = null;
 
+    /**
+     * Adds another description line to this error.
+     *
+     * @param description Description line to add
+     * @return This object for chainable calls
+     */
     public RDAPError addDescription(String description)
     {
         if(this.description == null)
@@ -22,29 +33,63 @@ public class RDAPError
         return this;
     }
 
+    /**
+     * Provides a list of description lines set for this object.
+     *
+     * If no descriptions have been set then this function returns null.
+     *
+     * @return Descriptions lines set on this error
+     */
     @JsonInclude(JsonInclude.Include.NON_NULL)
     public List<String> getDescription()
     {
         return description;
     }
 
+    /**
+     * Provides the error code set for this object.
+     *
+     * If no error code has been set than this function returns null.
+     *
+     * @return Error code set on this object
+     */
     public String getErrorCode()
     {
         return errorCode;
     }
 
+    /**
+     * Provides the title set for this object.
+     *
+     * If no title has been set than this function returns null.
+     *
+     * @return Title set on this object
+     */
     @JsonInclude(JsonInclude.Include.NON_NULL)
     public String getTitle()
     {
         return title;
     }
 
+    /**
+     * Sets the error code for this object and converts the supplied int to a
+     * string for RDAP.
+     *
+     * @param errorCode Error code to set
+     * @return This object for chainable calls
+     */
     public RDAPError setErrorCode(int errorCode)
     {
         this.errorCode = Integer.toString(errorCode);
         return this;
     }
 
+    /**
+     * Sets the title for this RDAPError object.
+     *
+     * @param title Title to set
+     * @return This object for chainable calls
+     */
     public RDAPError setTitle(String title)
     {
         this.title = title;
