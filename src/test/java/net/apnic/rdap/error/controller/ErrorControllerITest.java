2cb6ddd34f76012c17838aa3d222f8ddcc4eb2d9
Thomas Miller <tmiller@apnic.net>
2017-07-21 10:59:42 +1000

    Tests for rdap json conformance.

--- src/test/java/net/apnic/rdap/error/controller/ErrorControllerITest.java
+++ src/test/java/net/apnic/rdap/error/controller/ErrorControllerITest.java
@@ -1,8 +1,13 @@
 package net.apnic.rdap.error.controller;
 
+import static com.jayway.jsonpath.matchers.JsonPathMatchers.*;
+
 import java.util.stream.Stream;
 
+import static org.hamcrest.Matchers.*;
+
 import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertThat;
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.Arguments;
 import org.junit.jupiter.params.provider.MethodSource;
@@ -33,28 +38,39 @@ public class ErrorControllerITest
 
         badPaths.forEach(badPath ->
         {
-            ResponseEntity<Void> response = new TestRestTemplate()
+            ResponseEntity<String> response = new TestRestTemplate()
                 .getForEntity("http://localhost:" + this.serverPort + badPath,
-                              Void.class);
+                              String.class);
 
             assertEquals(badPath, 400, response.getStatusCodeValue());
+            assertThat(response.getBody(), hasJsonPath("$.errorCode",
+                                                       equalTo("400")));
+            assertThat(response.getBody(), hasJsonPath("$.title",
+                                                       equalTo("Bad Request")));
+            assertThat(response.getBody(), hasJsonPath("$.rdapConformance",
+                                                       contains("rdap_level_0")));
         });
     }
 
     @Test
     public void returns404ForMalformedRequests()
     {
-        Stream.of("/doesnotexist",
-                  "/entity/doesnotexist",
+        Stream.of("/entity/doesnotexist",
                   "/domain/apnic.example",
                   "/nameserver/ns1.apnic.example")
         .forEach(nonPath ->
         {
-            ResponseEntity<Void> response = new TestRestTemplate()
+            ResponseEntity<String> response = new TestRestTemplate()
                 .getForEntity("http://localhost:" + this.serverPort + nonPath,
-                              Void.class);
+                              String.class);
 
             assertEquals(nonPath, 404, response.getStatusCodeValue());
+            assertThat(response.getBody(), hasJsonPath("$.errorCode",
+                                                       equalTo("404")));
+            assertThat(response.getBody(), hasJsonPath("$.title",
+                                                       equalTo("Not Found")));
+            assertThat(response.getBody(), hasJsonPath("$.rdapConformance",
+                                                       contains("rdap_level_0")));
         });
     }
 }
