498f0307c08e2f058ea7a3e6b8904837009e85a9
Thomas Miller <tmiller@apnic.net>
2017-07-21 14:02:00 +1000

    Consolodation of error checking logic

--- src/test/java/net/apnic/rdap/error/controller/ErrorControllerITest.java
+++ src/test/java/net/apnic/rdap/error/controller/ErrorControllerITest.java
@@ -1,10 +1,14 @@
 package net.apnic.rdap.error.controller;
 
 import static com.jayway.jsonpath.matchers.JsonPathMatchers.*;
+import com.jayway.jsonpath.matchers.JsonPathMatchers;
 
 import java.util.stream.Stream;
 
+import org.hamcrest.Matcher;
 import static org.hamcrest.Matchers.*;
+import org.hamcrest.Matchers;
+import org.hamcrest.object.HasToString;
 
 import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.assertThat;
@@ -28,6 +32,43 @@ public class ErrorControllerITest
     @LocalServerPort
     private int serverPort;
 
+    private Matcher<ResponseEntity<String>> isValidRDAPResponse =
+        Matchers.allOf(
+            hasProperty("headers", hasProperty("content-type", HasToString.hasToString("application/rdap+json"))),
+            hasProperty("body", Matchers.allOf(
+                JsonPathMatchers.isJson(),
+                JsonPathMatchers.hasJsonPath("$.description"),
+                JsonPathMatchers.hasJsonPath("$.errorCode"),
+                JsonPathMatchers.hasJsonPath("$.title"),
+                JsonPathMatchers.hasJsonPath("$.notices"),
+                JsonPathMatchers.hasJsonPath("$.rdapConformance", contains("rdap_level_0"))
+                )
+            )
+        );
+
+    private Matcher<ResponseEntity<String>> is400RDAPErrorResponse =
+        Matchers.allOf(
+            hasProperty("statusCodeValue", Matchers.is(400)),
+            isValidRDAPResponse,
+            hasProperty("body", Matchers.allOf(
+                JsonPathMatchers.hasJsonPath("$.errorCode", Matchers.is("400")),
+                JsonPathMatchers.hasJsonPath("$.title", Matchers.is("Bad Request"))
+                )
+            )
+        );
+
+    private Matcher<ResponseEntity<String>> is404RDAPErrorResponse =
+        Matchers.allOf(
+            hasProperty("statusCodeValue", Matchers.is(404)),
+            isValidRDAPResponse,
+            hasProperty("body", Matchers.allOf(
+                JsonPathMatchers.hasJsonPath("$.errorCode", Matchers.is("404")),
+                JsonPathMatchers.hasJsonPath("$.title", Matchers.is("Not Found"))
+                )
+            )
+        );
+
+
     @Test
     public void returns400ForMalformedRequests()
     {
@@ -42,13 +83,7 @@ public class ErrorControllerITest
                 .getForEntity("http://localhost:" + this.serverPort + badPath,
                               String.class);
 
-            assertEquals(badPath, 400, response.getStatusCodeValue());
-            assertThat(response.getBody(), hasJsonPath("$.errorCode",
-                                                       equalTo("400")));
-            assertThat(response.getBody(), hasJsonPath("$.title",
-                                                       equalTo("Bad Request")));
-            assertThat(response.getBody(), hasJsonPath("$.rdapConformance",
-                                                       contains("rdap_level_0")));
+            assertThat(response, is400RDAPErrorResponse);
         });
     }
 
@@ -64,13 +99,7 @@ public class ErrorControllerITest
                 .getForEntity("http://localhost:" + this.serverPort + nonPath,
                               String.class);
 
-            assertEquals(nonPath, 404, response.getStatusCodeValue());
-            assertThat(response.getBody(), hasJsonPath("$.errorCode",
-                                                       equalTo("404")));
-            assertThat(response.getBody(), hasJsonPath("$.title",
-                                                       equalTo("Not Found")));
-            assertThat(response.getBody(), hasJsonPath("$.rdapConformance",
-                                                       contains("rdap_level_0")));
+            assertThat(response, is404RDAPErrorResponse);
         });
     }
 }
