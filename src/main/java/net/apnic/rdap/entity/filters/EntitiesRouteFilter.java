3327de18f7e002de3419f16cd9140b7eee78a111
Thomas Miller <tmiller@apnic.net>
2017-09-08 13:13:40 +1000

    Adds support for passing through entities search

---
+++ src/main/java/net/apnic/rdap/entity/filters/EntitiesRouteFilter.java
@@ -0,0 +1,59 @@
+package net.apnic.rdap.entity.filters;
+
+import net.apnic.rdap.authority.RDAPAuthority;
+import net.apnic.rdap.directory.Directory;
+import net.apnic.rdap.entity.Entity;
+import net.apnic.rdap.error.MalformedRequestException;
+import net.apnic.rdap.filter.filters.RDAPPathRouteFilter;
+import net.apnic.rdap.filter.RDAPRequestPath;
+import net.apnic.rdap.filter.RDAPRequestType;
+import net.apnic.rdap.resource.ResourceNotFoundException;
+
+/**
+ * Filter for handling entity search path segments in RDAP requests
+ */
+public class EntitiesRouteFilter
+    extends RDAPPathRouteFilter
+{
+    private static final int NO_REQUEST_PARAMS = 0;
+
+    /**
+     * Main constructor which takes the Directory to use for locating entity
+     * authorities.
+     */
+    public EntitiesRouteFilter(Directory directory)
+    {
+        super(directory);
+    }
+
+    /**
+     * Main run method for filter which takes the incoming request and finds an
+     * entity authority.
+     *
+     * @see RDAPPathRouteFilter
+     */
+    @Override
+    public RDAPAuthority runRDAPFilter(RDAPRequestPath path)
+        throws ResourceNotFoundException, MalformedRequestException
+    {
+        String[] args = path.getRequestParams();
+
+        if(args.length != NO_REQUEST_PARAMS)
+        {
+            throw new MalformedRequestException(
+                "Not enough arguments for domain path segment");
+        }
+
+        return getDirectory()
+            .getEntityAuthority(new Entity(null));
+    }
+
+    /**
+     * {@inheritDocs}
+     */
+    @Override
+    public RDAPRequestType supportedRequestType()
+    {
+        return RDAPRequestType.ENTITIES;
+    }
+}
