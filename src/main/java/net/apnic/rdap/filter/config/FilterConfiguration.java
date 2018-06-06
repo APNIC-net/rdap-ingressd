17bffd4d24cf41ccbf3c9c17b2b5718f14858186
Thomas Miller <tmiller@apnic.net>
2018-06-06 11:26:45 +1000

    Adds cors support to ingressd

--- src/main/java/net/apnic/rdap/filter/config/FilterConfiguration.java
+++ src/main/java/net/apnic/rdap/filter/config/FilterConfiguration.java
@@ -1,9 +1,18 @@
 package net.apnic.rdap.filter.config;
 
+import java.util.Arrays;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
 import net.apnic.rdap.filter.filters.RDAPPreFilter;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.server.ServletServerHttpResponse;
+import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.cors.CorsUtils;
+import org.springframework.web.filter.CorsFilter;
 
 /**
  *
@@ -11,6 +20,35 @@ import org.springframework.context.annotation.Configuration;
 @Configuration
 public class FilterConfiguration
 {
+    @Bean
+    public CorsFilter corsFilter()
+    {
+        CorsFilter filter = new CorsFilter((HttpServletRequest req) -> {
+            return new CorsConfiguration();
+        });
+        filter.setCorsProcessor(
+            (CorsConfiguration c, HttpServletRequest req,
+             HttpServletResponse res) -> {
+
+            ServletServerHttpResponse r = new ServletServerHttpResponse(res);
+            System.out.println(r.getHeaders().getAccessControlAllowOrigin());
+
+            if(CorsUtils.isCorsRequest(req) == false ||
+               res.getHeaders(HttpHeaders.ACCESS_CONTROL_ALLOW_ORIGIN).isEmpty() == false) {
+                return true;
+            }
+
+            if(CorsUtils.isPreFlightRequest(req)) {
+                res.setHeader(HttpHeaders.ACCESS_CONTROL_ALLOW_METHODS,
+                    "GET, HEAD, OPTION");
+            }
+
+            res.setHeader(HttpHeaders.ACCESS_CONTROL_ALLOW_ORIGIN, "*");
+            return true;
+        });
+        return filter;
+    }
+
     @Bean
     public RDAPPreFilter rdapPreFilter()
     {
---
+++ src/test/java/net/apnic/rdap/filter/CorsControllerTest.java
@@ -0,0 +1,93 @@
+package net.apnic.rdap.filter;
+
+import java.net.URI;
+import java.util.Arrays;
+import java.util.stream.Stream;
+
+import org.hamcrest.collection.IsMapContaining;
+import org.hamcrest.Matcher;
+import org.hamcrest.Matchers;
+
+import static org.junit.Assert.assertThat;
+import org.junit.runner.RunWith;
+import org.junit.Test;
+
+import org.springframework.boot.context.embedded.LocalServerPort;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.context.SpringBootTest.WebEnvironment;
+import org.springframework.boot.test.web.client.TestRestTemplate;
+import org.springframework.http.RequestEntity;
+import org.springframework.http.ResponseEntity;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest(webEnvironment = WebEnvironment.RANDOM_PORT)
+public class CorsControllerTest
+{
+    @LocalServerPort
+    private int serverPort;
+
+    private Matcher<ResponseEntity<String>> isCorsResponse =
+        Matchers.allOf(
+            Matchers.hasProperty("headers",
+                IsMapContaining.hasEntry("Access-Control-Allow-Origin",
+                                         Arrays.asList("*"))));
+
+    @Test
+    public void returnsCorsHeadersGet()
+        throws Exception
+    {
+        Stream<String> corsPaths = Stream.of("/ip/10.5.5.5/32",
+                                             "/autnum/1234",
+                                             "/entity/apnic",
+                                             "/domain/cors.apnic.net",
+                                             "/nameserver/ns1.apnic.net",
+                                             "/help");
+
+        corsPaths.forEach(corsPath -> {
+            RequestEntity<Void> request;
+            try {
+                request = RequestEntity
+                    .get(new URI("http://localhost:" + this.serverPort + corsPath))
+                    .header("Origin", "https://cors.apnic.net")
+                    .build();
+            } catch(Exception ex) {
+                throw new RuntimeException(ex);
+            }
+
+            ResponseEntity<String> response = new TestRestTemplate()
+                .exchange(request, String.class);
+
+            assertThat(response, isCorsResponse);
+        });
+    }
+
+    @Test
+    public void returnsCorsHeadersHead()
+        throws Exception
+    {
+        Stream<String> corsPaths = Stream.of("/ip/10.5.5.5/32",
+                                             "/autnum/1234",
+                                             "/entity/apnic",
+                                             "/domain/cors.apnic.net",
+                                             "/nameserver/ns1.apnic.net",
+                                             "/help");
+
+        corsPaths.forEach(corsPath -> {
+            RequestEntity<Void> request;
+            try {
+                request = RequestEntity
+                    .head(new URI("http://localhost:" + this.serverPort + corsPath))
+                    .header("Origin", "https://cors.apnic.net")
+                    .build();
+            } catch(Exception ex) {
+                throw new RuntimeException(ex);
+            }
+
+            ResponseEntity<String> response = new TestRestTemplate()
+                .exchange(request, String.class);
+
+            assertThat(response, isCorsResponse);
+        });
+    }
+}
